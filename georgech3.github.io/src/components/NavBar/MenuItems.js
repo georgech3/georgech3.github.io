28f4398ecfac57d167b7e79cf68a266dc9298d01
georgech3 <zx1256910@gmail.com>
2021-07-04 16:13:19 +0800

    add fonts and basic nav

---
+++ georgech3.github.io/src/components/NavBar/MenuItems.js
@@ -0,0 +1,21 @@
+export const MenuItems = [{
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Article',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Demo',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
