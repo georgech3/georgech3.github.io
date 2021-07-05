4933e3b774144e6b4fe9b35c77d08ead9e2bac76
georgech3 <zx1256910@gmail.com>
2021-07-05 17:24:21 +0800

    add responsive menu

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { MenuItems } from "./MenuItems"
+import { ReactComponent as MenuIcon } from "./menu.svg";
+import { ReactComponent as CloseMenu } from "./close-menu.svg";
 import './NavBar.css'
 
 class NavBar extends Component {
-    state = { clicked: false }
+    state = { clicked: false, setClick: false }
 
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
+    handleClick = () => this.setState({ clicked: !this.state.clicked });
+    closeMobileMenu = () => this.setState({ setClick: !this.state.setClick });
 
     render() {
         return (
@@ -20,13 +21,17 @@ class NavBar extends Component {
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                                <a className={item.cName} href={item.url}>
+                                <a className={item.cName} href={item.url} onClick={this.closeMobileMenu}>
                                     {item.title}
                                 </a>
                             </li>
                         )
                     })}
                 </ul>
+                <div className="mobile-menu" onClick={this.handleClick}>
+                    {this.clicked ?
+                        (<CloseMenu className="mobile-option" />) : (<MenuIcon className="menu-icon" />)}
+                </div>
             </nav >
         )
     }
